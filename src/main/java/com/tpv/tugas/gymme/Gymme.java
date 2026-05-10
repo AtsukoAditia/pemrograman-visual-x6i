8923c570612eb241c78308f077c7b7bfc27211d0
atsukoaditia <atsuko.aditia@gmail.com>
2026-05-10 17:14:58 +0700

    first commit

---
+++ src/main/java/com/tpv/tugas/gymme/Gymme.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tpv.tugas.gymme;
+
+/**
+ *
+ * @author atsukoaditia
+ */
+public class Gymme {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/koneksi/koneksi.java
@@ -0,0 +1,31 @@
+package koneksi;
+import java.sql.*;
+
+/**
+ *
+ * @author atsukoaditia
+ */
+public class koneksi {
+    private Connection koneksi;
+
+    public Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Berhasil load driver MySQL");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Gagal load driver: " + ex.getMessage());
+            return null;
+        }
+
+        String url = "jdbc:mysql://localhost:3306/latihan_pv";
+        try {
+            koneksi = DriverManager.getConnection(url, "root", "");
+            System.out.println("Berhasil koneksi database");
+        } catch (SQLException ex) {
+            System.out.println("Koneksi database gagal: " + ex.getMessage());
+            koneksi = null;
+        }
+
+        return koneksi;
+    }
+}
